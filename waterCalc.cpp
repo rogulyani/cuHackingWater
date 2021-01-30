dddc988b45bb14f6ad9996bb5c733151b9618101
CwithEmma <78239505+CwithEmma@users.noreply.github.com>
2021-01-30 13:17:42 -0500

    Add files via upload

--- waterCalc.cpp
+++ waterCalc.cpp
@@ -5,13 +5,23 @@
 #include <stdio.h>
 using namespace std;
 
-float waterCalc(float weight,int time)
+float waterCalc(float weight,int time, int caffine, int alchohol)
 {
 	float ret = weight * 2 / 3;
 	if  (time != NULL)
 	{
 		float waterPlus = addWater(time);
 		ret = ret + waterPlus;
+	}
+	if (caffine != NULL)
+	{
+		float waterPlus2 = AddMore(caffine);
+		ret = ret + waterPlus2;
+	}
+	if (alchohol != NULL)
+	{
+		float waterPlus3 = AddMore(alchohol);
+		ret = ret + waterPlus3;
 	}
 	return ret;
 }
@@ -22,6 +32,16 @@ float addWater(int time)
 	for (int i = 0; i < time; i + 5)
 	{
 		counter += 2.4;
+	}
+	return counter;
+}
+
+float AddMore(int number)
+{
+	float counter = 0;
+	for (int i = 0; i < number; i++)
+	{
+		counter++;
 	}
 	return counter;
 }
