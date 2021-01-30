fb0bb806dc7ceaf6a0e73138dfdef375c835860a
CwithEmma <78239505+CwithEmma@users.noreply.github.com>
2021-01-30 16:02:14 -0500

    Update waterCalc.cpp

--- waterCalc.cpp
+++ waterCalc.cpp
@@ -2,14 +2,14 @@
 
 void waterCal(drinkerType* ret)
 {
-	ret->total = ret->weight * 2 / 3;
+	ret->total = ret->weight * 0.6;
 	ret->total += ret->caffine * 8;
 	ret->total += ret->alcholo * 8;
 
 	if (ret->timeWorkOut != NULL)
 	{
 		ret->timeWorkOut = addWater(ret->timeWorkOut);
-		ret->total += ret->total + ret->timeWorkOut;
+		ret->total += ret->timeWorkOut;
 	}
 	
 }
