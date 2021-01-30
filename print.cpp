050f823ed94d700318ff6c08c8380b8967e8a64a
CwithEmma <78239505+CwithEmma@users.noreply.github.com>
2021-01-30 13:42:00 -0500

    Add files via upload

---
+++ print.cpp
@@ -0,0 +1,19 @@
+#include "defs.h" 
+
+void printfun(water* ret )
+{
+	if (ret->caffine != NULL)
+	{
+		printf("The amount added to your total based on your cups of caffine is: %p \n", ret->caffine);
+	}
+	if (ret->alcholo != NULL)
+	{
+		printf("The amount add to your total based on your alcohol consumption is %p \n", ret->alcholo);
+	}
+	if (ret->timeWorkOut != NULL)
+	{
+		printf("The amount add to your total based on your time worked out is %p \n", ret->timeWorkOut);
+	}
+
+	printf("The total amount of water you should be consuming is: %p\n", ret->total);
+}
