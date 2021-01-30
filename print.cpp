3185ed8ddd6fb0a0a7430631620ef8557a2c1076
CwithEmma <78239505+CwithEmma@users.noreply.github.com>
2021-01-30 14:47:43 -0500

    Update print.cpp

--- print.cpp
+++ print.cpp
@@ -4,16 +4,16 @@ void printfun(drinkerType* ret )
 {
 	if (ret->caffine != NULL)
 	{
-		printf("The amount added to your total based on your cups of caffine is: %p \n", ret->caffine);
+		printf("The amount added to your total based on your cups of caffine is: %i \n", ret->caffine);
 	}
 	if (ret->alcholo != NULL)
 	{
-		printf("The amount add to your total based on your alcohol consumption is %p \n", ret->alcholo);
+		printf("The amount add to your total based on your alcohol consumption is %i \n", ret->alcholo);
 	}
 	if (ret->timeWorkOut != NULL)
 	{
-		printf("The amount add to your total based on your time worked out is %p \n", ret->timeWorkOut);
+		printf("The amount add to your total based on your time worked out is %i \n", ret->timeWorkOut);
 	}
 
-	printf("The total amount of water you should be consuming is: %p\n", ret->total);
+	printf("The total amount of water you should be consuming is: %f\n", ret->total);
 }
