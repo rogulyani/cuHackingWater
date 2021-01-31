c50dd46063c6b517f7cbf0f8961dc13978a98d77
CwithEmma <78239505+CwithEmma@users.noreply.github.com>
2021-01-30 22:30:04 -0500

    Update waterCalc.cpp

--- CuHackingWater/waterCalc.cpp
+++ CuHackingWater/waterCalc.cpp
@@ -1,5 +1,9 @@
 #include "defs.h" 
 
+/// <summary>
+/// Finds the user dayily average of water needed to be consumed
+/// </summary>
+/// <param name="ret"> pointer to the struct </param>
 void waterCal(drinkerType* ret)
 {
 	ret->total = ret->weight * 0.6;
@@ -14,6 +18,11 @@ void waterCal(drinkerType* ret)
 
 }
 
+/// <summary>
+/// Determines the added water need for how long they have worked out
+/// </summary>
+/// <param name="time">the length of the workout</param>
+/// <returns>how many ounces of added water needed </returns>
 float addWater(int time)
 {
 	float counter = 0;
